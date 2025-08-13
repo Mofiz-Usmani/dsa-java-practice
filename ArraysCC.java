fdea0200da5fde854670b570cd0d100bb1d3de08
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-13 15:30:45 +0530

    Initial commit - added DSA Java files

---
+++ ArraysCC.java
@@ -0,0 +1,17 @@
+import java.util.Arrays; // Import this to use Arrays methods
+
+public class ArraysCC {
+    public static void main(String[] args) {
+        int marks[] = new int[50]; // default values will be 0
+        int numbers[] = {1, 2, 3};
+        int moreNumbers[] = {4, 5, 6};
+        String fruits[] = {"apple", "orange", "mango"};
+
+        // Printing arrays
+        System.out.println(marks);
+        // System.out.println(Arrays.toString(marks));
+        // System.out.println(Arrays.toString(numbers));
+        // System.out.println(Arrays.toString(moreNumbers));
+        // System.out.println(Arrays.toString(fruits));
+    }
+}
