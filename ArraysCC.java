bc9204d6b04ece8f3ea314bfde6ff25d909d83c5
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-13 19:41:30 +0530

    linear search

--- ArraysCC.java
+++ ArraysCC.java
@@ -1,24 +1,49 @@
 import java.util.*;
 
 public class ArraysCC {
-    public static void update(int marks[], int nonChangable) {
-        nonChangable = 10;
-        System.out.println(nonChangable);
-        for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+    // public static void update(int marks[], int nonChangable) {
+    //     nonChangable = 10;
+    //     System.out.println(nonChangable);
+    //     for(int i=0; i<marks.length; i++){
+    //         marks[i] = marks[i] + 1;
+    //     }
+    // }
+
+// -------------------------------------------------
+
+    public static int linearSearch(int arr[], int key) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == key){
+                return i;
+            }
         }
+        return -1;
     }
     public static void main(String[] args) {
-        int marks[] = {97, 98, 99};
-        int nonChangable = 5;
-        update(marks, nonChangable);
-        System.out.println(nonChangable);
+        int arr[] = {2,4,6,8,10,12,14,16}; 
+        int key = 10;
 
-        //print marks 
-        for(int i=0; i<marks.length; i++) {
-            System.out.print(marks[i]+" ");
+        int index = linearSearch(arr, key);
+        if(index == -1) {
+            System.out.println("Not Found");
+        }else {
+            System.out.println("Key is at index : "+index);
         }
-        System.out.println();
+
+// -------------------------------------------------
+
+        // int marks[] = {97, 98, 99};
+        // int nonChangable = 5;
+        // update(marks, nonChangable);
+        // System.out.println(nonChangable);
+
+        // //print marks 
+        // for(int i=0; i<marks.length; i++) {
+        //     System.out.print(marks[i]+" ");
+        // }
+        // System.out.println();
+
+// -------------------------------------------------
 
         // int marks[] = new int[100]; 
 
