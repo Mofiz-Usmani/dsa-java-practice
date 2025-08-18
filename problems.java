255a754fa375a0a48ee1e69bc1d601ad3cf7ae72
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-18 11:24:27 +0530

    triplets:

--- problems.java
+++ problems.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class problems {
 
     // Trapped Water
@@ -64,8 +66,34 @@ public class problems {
 // }
 
 // ----------------------------------------------------------------------------------------
+public static void triplets(int nums[]){
+    Arrays.sort(nums);  // Sort the array first
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                // Skip duplicate values for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                for (int k = j + 1; k < nums.length; k++) {
+                    // Skip duplicate values for k
+                    if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+                    }
+                }
+            }
+        }
+}
+
 
 public static void main(String[] args) {
+    //Triplets 
+    int nums[] = {-1, 0 , 1, 2, -1, -4};
+    triplets(nums);
+
 
 // ----------------------------------------------------------------------------------------
 
