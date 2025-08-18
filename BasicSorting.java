dc7b9743870e3b52f67838a643a2d796e2f6eee6
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-18 15:43:10 +0530

    insertion sort

--- BasicSorting.java
+++ BasicSorting.java
@@ -37,9 +37,22 @@ public class BasicSorting {
         }
     }
 
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i]; 
+            int prev = i-1; 
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
 public static void main(String args[]){
-    int arr[] = {5,4,1,3,2};
-    selectionSort(arr);
+    int arr[] = {3,2,1,5,4};
+    insertionSort(arr);
+    // selectionSort(arr);
     printArr(arr);
 // -----------------------------------------------------------------------------------------
 
