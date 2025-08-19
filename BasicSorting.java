4903023d04f9fe45271bbe67091978719d3dbd23
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-19 15:53:11 +0530

    built-in sorting

--- BasicSorting.java
+++ BasicSorting.java
@@ -15,7 +15,7 @@ public class BasicSorting {
         }
     }
 
-    public static void printArr(Integer arr[]){
+    public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
@@ -52,12 +52,35 @@ public class BasicSorting {
         }
     }
 
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+
+        int j=0; 
+        for(int i=0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i; 
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
 public static void main(String args[]){
-    Integer arr[] = {5,4,1,3,2};
+    // int arr[] = {5,4,1,3,2};
+    int arr[] = {1,4,1,3,2,4,3,7};
     // insertionSort(arr);
     // selectionSort(arr);
     // Arrays.sort(arr,0,3);
-    Arrays.sort(arr, Collections.reverseOrder());
+    // Arrays.sort(arr,0,3, Collections.reverseOrder());
+    countingSort(arr);
     printArr(arr);
 // -----------------------------------------------------------------------------------------
 
