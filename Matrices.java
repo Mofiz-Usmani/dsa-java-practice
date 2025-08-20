07d8ad34d7e4febb4899fb7451d08bc602616fb0
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-20 16:27:19 +0530

    Spiral Matrix Print

--- Matrices.java
+++ Matrices.java
@@ -1,38 +1,73 @@
 import java.util.*;
 
 public class Matrices {
-    public static boolean search(int matrix[][], int key){
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j] == key){
+
+    // Search key in matrix
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
                     System.out.println("Found at cell (" + i + "," + j + ")");
-                return true;
+                    return true;
                 }
             }
         }
         System.out.println("Key not found");
         return false;
     }
-    public static void main(String args[]){
-        int matrix[][] = new int[3][3];
-        int n = matrix.length, m = matrix[0].length;
 
-        Scanner sc = new Scanner(System.in);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                matrix[i][j] = sc.nextInt();
+    // Spiral print method
+    public static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
             }
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // bottom
+            if (startRow < endRow) {
+                for (int j = endCol - 1; j >= startCol; j--) {
+                    System.out.print(matrix[endRow][j] + " ");
+                }
+            }
+
+            // left
+            if (startCol < endCol) {
+                for (int i = endRow - 1; i > startRow; i--) {
+                    System.out.print(matrix[i][startCol] + " ");
+                }
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
         }
         System.out.println();
+    }
 
-        //Output : 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-            System.out.println();
-        search(matrix, 5);
+    public static void main(String args[]) {
+        int matrix[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        // Spiral print
+        printSpiral(matrix);
+
+        // Search example
+        search(matrix, 10);
     }
 }
