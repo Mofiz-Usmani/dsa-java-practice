1b837953429c504528020edbace5e978d0a144f2
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-20 13:00:52 +0530

    2D Array

--- Matrices.java
+++ Matrices.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 public class Matrices {
     public static void search(int matrix[][]){
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                     // System.out.println("Found at cell (" + i + "," + j + ")");
                     // return true;
-                if(max > matrix[i][j]){
+                if(max < matrix[i][j]){
                     max = matrix[i][j];
                 }
             }
-            System.out.println(max);
         }
-        // System.out.println(max);
+        System.out.println(max);
     }
     public static void main(String args[]){
         int matrix[][] = new int[3][3];
