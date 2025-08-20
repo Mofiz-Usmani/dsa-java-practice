16819ab47d4558eb469ee4e9fda459ff770d7af2
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-20 12:58:17 +0530

    2D Array

--- Matrices.java
+++ Matrices.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
 public class Matrices {
-    public static boolean search(int matrix[][], int key){
+    public static void search(int matrix[][]){
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j] == key) {
-                    System.out.println("Found at cell (" + i + "," + j + ")");
-                    return true;
+                    // System.out.println("Found at cell (" + i + "," + j + ")");
+                    // return true;
+                if(max > matrix[i][j]){
+                    max = matrix[i][j];
                 }
             }
+            System.out.println(max);
         }
-        System.out.println("Key not found");
-        return false;
+        // System.out.println(max);
     }
     public static void main(String args[]){
         int matrix[][] = new int[3][3];
@@ -31,6 +33,6 @@ public class Matrices {
             }
             System.out.println();
         }
-        search(matrix, 5);
+        search(matrix);
     }
 }
