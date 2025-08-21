ef221dc74153cc0c6d6fcb3fda1cbc84040ac9a3
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-21 11:37:00 +0530

    Diagonal Sum

--- Matrices.java
+++ Matrices.java
@@ -83,10 +83,9 @@ public class Matrices {
     }
     public static void main(String args[]) {
         int matrix[][] = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8}
         };
 
         // Spiral print
