5c5f33a20aacf7d75bf22984a1a446d2688c15e8
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-21 13:19:47 +0530

    presence of no of value in 2d array

--- Matrices.java
+++ Matrices.java
@@ -101,13 +101,27 @@ public class Matrices {
         System.out.println("Key not found");
         return false;
     }
+
+    public static void noofvalue(int matrix[][], int target) {
+        int count = 0;
+
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == target) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Number of occurrences of " + target + " : " + count);
+    }
+
+
     public static void main(String args[]) {
         int matrix[][] = {
-            {10, 20, 30, 40},
-            {15, 25, 35, 45},
-            {27, 29, 37, 48},
-            {32, 33, 39, 50}
+            {4,7,8},
+            {8,8,7}
         };
+        int target = 8;
         int key = 33;
 
         // Spiral print
@@ -122,6 +136,9 @@ public class Matrices {
 
 
         // Staircase search
-        staircaseSearch(matrix, key);
+        // staircaseSearch(matrix, key);
+
+        // Count occurrences of a value
+        noofvalue(matrix, target);
     }
 }
