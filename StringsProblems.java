64fbf55a4686f11f085b47cb7821836135ebcbfa
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-22 18:39:01 +0530

    Shortest Path

--- StringsProblems.java
+++ StringsProblems.java
@@ -11,8 +11,35 @@ public class StringsProblems {
         return true;
     }
 
+    // Shortest Path
+    public static float getPath(String path){
+        int x=0, y=0;
+        
+        for(int i=0; i<path.length(); i++){
+                if(path.charAt(i) == 'N'){
+                    y++;
+                } else if(path.charAt(i ) == 'S'){
+                    y--;
+                } else if(path.charAt(i) == 'E'){
+                    x++;
+                } else if(path.charAt(i) == 'W'){
+                    x--;
+                }     
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        double result = Math.sqrt(X2 + Y2);
+        return (float)result;
+    }
+
     public static void main(String[] args){
+        String path = "WNEENESENNN";
         String str = "lol";
-        System.out.println(isPalindrome(str));
+
+        // Palindrome
+        // System.out.println(isPalindrome(str));
+
+        // Shortest path
+        System.out.println(getPath(path));
     }
 }
