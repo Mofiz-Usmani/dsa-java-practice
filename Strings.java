6c1322d498a303cd141488b7ffb261d20100c3c6
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-22 16:02:00 +0530

    String in Java

--- Strings.java
+++ Strings.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class Strings {
+    public static void printLetters(String str){
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args){
         char arr[] = {'a', 'b', 'c', 'd', 'e'};
         String str = "abcde";
@@ -8,15 +15,17 @@ public class Strings {
 
         // Strings are IMMUTABLE
 
-        Scanner sc = new Scanner(System.in);
-        String name;
-        name = sc.nextLine();
-        System.out.println(name.length());  // length of string
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println(name.length());  // length of string
 
         // Concatenation
         String firstName = "John";
         String lastName = "Doe";
         String fullName = firstName + " " + lastName;
         System.out.println(fullName);
+
+        printLetters(fullName);
     }
 }
