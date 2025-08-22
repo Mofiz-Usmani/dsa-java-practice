d3b62bab5e2c69afdefa4315fd95c4b07d53f717
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-22 14:49:46 +0530

    String in Java

--- Strings.java
+++ Strings.java
@@ -2,5 +2,8 @@ public class Strings {
     public static void main(String[] args[]){
         char arr[] = {'a', 'b', 'c', 'd', 'e'};
         String str = "abcde";
+        String str2 = new String("xyz");
+
+        // Strings are IMMUTABLE
     }
 }
