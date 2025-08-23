a3e61813be4a34e6ae51354d158190ff38657ec1
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-23 12:15:37 +0530

    Substring

--- StringsProblems.java
+++ StringsProblems.java
@@ -32,8 +32,18 @@ public class StringsProblems {
         return (float)result;
     }
 
-    public static void main(String[] args){
+    public static String substring(String str, int si, int ei){
+        String substr = "";
+        for(int i=si; i<ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
 
+    public static void main(String[] args){
+        String str = "HelloWorld";
+
+        System.out.println(substring(str, 0, 4));
 
         // String path = "WNEENESENNN"; // Shortest path
         // String str = "lol";  // Palindrome
