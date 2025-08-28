0323f405ec35b0a170e3ac9d52c08a8b78b4dbf1
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-28 15:36:33 +0530

    Odd or Even

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
 public class BitManipulation {
+
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if((n & bitMask) == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println((6>>1)); 
+        oddOrEven(3);
+        oddOrEven(11);
+        oddOrEven(16);
     }
 }
