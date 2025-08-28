9288e341d3c98c0dee65a2d87bd300a6d3dadc06
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-28 15:24:40 +0530

    binary right shift operator

--- BitManipulation.java
+++ BitManipulation.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class BitManipulation {
     public static void main(String[] args) {
-        System.out.println((5<<2)); 
+        System.out.println((6>>1)); 
     }
 }
