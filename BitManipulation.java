3051bd531e7c9b56e0a90533a986f3eb96ae00f9
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-29 15:02:37 +0530

    get ith bit

--- BitManipulation.java
+++ BitManipulation.java
@@ -10,9 +10,26 @@ public class BitManipulation {
             System.out.println("Odd");
         }
     }
+
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+
+        if((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+
     public static void main(String[] args) {
-        oddOrEven(3);
-        oddOrEven(11);
-        oddOrEven(16);
+        System.out.println(getIthBit(10, 2));
+
+
+
+        // Odd or Even
+        // oddOrEven(3);
+        // oddOrEven(11);
+        // oddOrEven(16);
     }
 }
