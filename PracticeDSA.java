53a1294afa51696eb56ea8345025f7ceb0517fe2
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-08-31 16:41:06 +0530

    star pattern

--- PracticeDSA.java
+++ PracticeDSA.java
@@ -1,13 +1,31 @@
 public class PracticeDSA {
     public static void main(String[] args) {
-        
-        // Star Pattern
 
+        // // Star Pattern #1:
+        // for(int i=1; i<=4; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // // Star Pattern #2:
+        // for(int i=1; i<=4; i++){
+        //     for(int j=4; j>i-1; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Star Pattern #3: 
         for(int i=1; i<=4; i++){
-            for(int j=4; j>i-1; j--){
-                System.out.print("*");
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
+
     }
 }
