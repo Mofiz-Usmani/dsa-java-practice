83b72121c0c6a9e54fcc822fac066da801e1f8d2
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-09-06 12:47:00 +0530

    Constructors in java

--- OOPS.java
+++ OOPS.java
@@ -149,11 +149,22 @@ class Student {
     int marks[];
 
     // Shallow Copy Constructor
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks = s1.marks;
+    // }
+
+
+    // Deep Copy Constructor
     Student(Student s1){
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
-        this.marks = s1.marks;
+        for(int i=0; i<marks.length; i++){
+            this.marks[i] = s1.marks[i]; 
+        }
     }
 
 
