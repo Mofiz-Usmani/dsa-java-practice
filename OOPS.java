b661d8b966741829775c89b3281b652dc8c43ef1
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-09-06 12:13:39 +0530

    Constructors in java

--- OOPS.java
+++ OOPS.java
@@ -125,9 +125,16 @@ public class OOPS {
         s1.name = "mofiz";
         s1.roll = 12;
         s1.password = "abc";
-
+        s1.marks[0] = 90;
+        s1.marks[1] = 80;
+        s1.marks[2] = 70;
+        
         Student s2 = new Student(s1);
         s2.password = "xyz";
+
+        for(int i=0; i<3; i++){
+            System.out.println(s2.marks[i]);
+        }
     }
 }
 
@@ -142,6 +149,7 @@ class Student {
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
+        this.marks = s1.marks;
     }
 
 
