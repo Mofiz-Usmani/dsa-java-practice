36d5f8dbba7e0689df29f3b0ceba74c9cc2c8322
Mofiz-Usmani <mofizusmani1@gmail.com>
2025-09-11 15:46:25 +0530

    print from 1 to n

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -1,14 +1,27 @@
 public class RecursionBasics {
+
+    // Print form n to 1
     public static void printDec(int n){
         if(n == 1){
-            System.out.print(n);
+            System.out.println(n);
             return;
         }
         System.out.print(n+" ");
         printDec(n-1);
     }
+
+    // Print from 1 to n
+    public static void printInc(int n) {
+        if(n == 1) {
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
-        int n = 10;
-        printDec(n);
+        int n = 5;
+        // printDec(n);
+        printInc(n);
     }
 }
