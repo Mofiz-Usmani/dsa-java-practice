70cb3741c3011ea2e1d6acccb174ad89110c91df
Jackal <mofizusmani1@gmail.com>
2025-09-12 08:59:07 +0530

    Print Sum of first n natural numbers

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -30,10 +30,23 @@ public class RecursionBasics {
         int fn = n * fact(n-1);
         return fn;
     }
+
+
+    // Print Sum of first n natural numbers
+    public static int calcSum(int n){
+        if(n == 1){
+            return 1;
+        }
+
+        int Snm1 = calcSum(n-1);
+        int Sn = n + Snm1;
+        return Sn;
+    }
     public static void main(String[] args) {
         int n = 5;
         // printDec(n);
         // printInc(n);
-        System.out.println(fact(n));
+        // System.out.println(fact(n));
+        System.out.println(calcSum(n));
     }
 }
