1582768b4766e0d2273be85c0a4ae2ccc65937f2
Jackal <mofizusmani1@gmail.com>
2025-09-22 14:54:44 +0530

    merge sort

--- DividenConquer.java
+++ DividenConquer.java
@@ -1,3 +1,15 @@
 public class DividenConquer {
     
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {6, 3, 9, 5, 2, 8};
+        printArr(arr);
+    }
 }
