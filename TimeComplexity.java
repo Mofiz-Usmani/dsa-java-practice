8105a898d3e2563cf5a09f13d267e272e3bf3f95
Jackal <mofizusmani1@gmail.com>
2025-09-30 08:18:42 +0530

    power 2 adn 3

--- TimeComplexity.java
+++ TimeComplexity.java
@@ -145,12 +145,45 @@ public class TimeComplexity {
 
 
 
-    // Power Function : 
+    // Power Function 1 : 
     public static int power(int a, int n){
         if(n == 0){
             return 1;
         }
         return a * power(a, n-1);
+    }
+
+
+
+    // Power Function 2: 
+    public static int power2(int a, int n){
+        if(n == 0){
+            return 1;
+        }
+        int halfPowerSq = power2(a, n/2) * power2(a, n/2);
+
+        if(n%2 != 0){
+            return a * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
+    // https://chatgpt.com/s/t_68da8ee5382081919c837b619b226a64
+
+
+
+    public static int power3(int a, int n){
+        if(n == 0){
+            return 1;
+        }
+        int halfPower = power3(a, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        if(n%2 != 0){
+            return a * halfPowerSq;
+        }
+
+        return halfPowerSq;
     }
 
     public static void main(String[] args){
@@ -189,7 +222,16 @@ public class TimeComplexity {
 
 
 
-        System.out.println(power(2, 2));
-        System.out.println(Math.pow(2, 2));
+        // System.out.println(power(2, 2)); Power Function 1
+        // System.out.println(Math.pow(2, 2));
+
+
+        // System.out.println(power2(2, 3));
+        // System.out.println(2/2);
+        // System.out.println(1/2);
+
+
+        System.out.println(power3(2,2));
+
     }
 }
