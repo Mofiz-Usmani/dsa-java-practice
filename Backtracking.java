575e31ad914813daff63ea0e7b493f241eee11ef
Jackal <mofizusmani1@gmail.com>
2025-10-02 14:15:02 +0530

    nQueens non attacking

--- Backtracking.java
+++ Backtracking.java
@@ -43,6 +43,35 @@ public class Backtracking {
         }
     }
 
+
+    //nQueens : 
+    public static void nQueens(char board[][], int row){
+        // Base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+
+        // Column loop
+        for(int j=0; j<board.length; j++){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        }
+    }
+
+
+    public static void printBoard(char board[][]){
+        System.out.println("--------------Chess Board--------------");
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                System.out.print(board[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
@@ -60,7 +89,20 @@ public class Backtracking {
         // https://chatgpt.com/s/t_68dbc5243f008191a9e0996e0821e483
 
 
-        String str = "abc";
-        findPermutation(str, "");
+        // String str = "ab";
+        // findPermutation(str, "");
+        // https://chatgpt.com/s/t_68de09b24c1881918e9f23b0ceede553
+
+
+        int n = 2;
+        char board[][] = new char[n][n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
+            }
+        }
+
+        nQueens(board, 0);
     }
 }
